c08b19180db179dfddce3b40362ca8c2ad842353
reninoz <zhangrenqweijun@icloud.com>
2017-05-29 20:24:10 +1000

    more

--- springboot-angularjs/src/main/resources/static/js/lease/controller/lease-controller.js
+++ springboot-angularjs/src/main/resources/static/js/lease/controller/lease-controller.js
@@ -7,8 +7,33 @@ app.controller('LeaseController', ['LeaseService', function (LeaseService) {
         LeaseService.allLeases()
             .then(function (result) {
                 self.allLease = (result !== 'null') ? result.data : [];
+                console.log('data returned size ' + self.allLease.length);
+                self.totalItems = self.allLease.length;
             });
     }
     self.getAllLease();
+    
+    self.data=self.allLease;
+    
+    self.viewby = 5;
+    console.log(self.allLease);
+    self.totalItems = self.data.length;
+    console.log('data.length: ' + self.totalItems);
+    self.currentPage = 1;
+    self.itemsPerPage = self.viewby;
+    self.maxSize = 5; //Number of pager buttons to show
+
+    self.setPage = function (pageNo) {
+      self.currentPage = pageNo;
+    };
+
+    self.pageChanged = function() {
+      console.log('Page changed to: ' + self.currentPage);
+    };
+
+  self.setItemsPerPage = function(num) {
+    self.itemsPerPage = num;
+    self.currentPage = 1; //reset to first paghe
+  };
 
 }]);
